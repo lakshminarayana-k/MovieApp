90153d7dff448cf944495b2298f0f4f7ccfdc087
VassarL <lakshminarayana@vassarlabs.com>
2024-09-08 03:59:37 +0530

    initilal commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MovieOverviewComponent } from './components/movie-overview/movie-overview.component';
+import { FormsModule } from '@angular/forms';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { FavouriteMoviesComponent } from './components/favourite-movies/favourite-movies.component';
+import { ResourceNotFoundComponent } from './components/resource-not-found/resource-not-found.component';
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieOverviewComponent,
+    LandingPageComponent,
+    FavouriteMoviesComponent,
+    ResourceNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
